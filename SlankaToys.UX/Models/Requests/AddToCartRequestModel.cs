9d84c3915c7df1b6a5a2dfabe9bdc17260a2132e
Dusan Marin <dusan.marin@live.com>
2021-10-10 15:36:11 +0200

    add to cart from product details - hook up with API

---
+++ SlankaToys.UX/Models/Requests/AddToCartRequestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlankaToys.UX.Models.Requests
+{
+    public class AddToCartRequestModel
+    {
+        public AddToCartRequestModel()
+        {
+
+        }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
