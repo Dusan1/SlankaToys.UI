d5aa66ba51fa18ac9ce9588022c0186b82e4a198
Dusan Marin <marin.dusan2@gmail.com>
2021-12-19 13:27:12 +0100

    update cart item

---
+++ SlankaToys.UX/Models/Requests/UpdateCartRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SlankaToys.UX.Models.Requests
+{
+    public class UpdateCartRequestModel
+    {
+        public UpdateCartRequestModel()
+        {
+        }
+
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
+
